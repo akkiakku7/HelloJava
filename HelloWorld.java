325a3ae42d8c2fa4e4050e3110c0d7b67413b357
akkiakku7 <akashmachary4@gmail.com>
2025-07-31 11:51:43 +0530

    HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
-public class HelloWorld{
-	public static void main(String[] args){
-			System.Out.Println("Hello World");
-	}
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins!");
+    }
 }
